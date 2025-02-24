98cb658f2d68e6036c67fc26ddd102f62ad363dc
SRLABREEZE <srlabreeze@gmail.com>
2025-02-24 05:18:42 -0500

    Update menu.js

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,3 +1,10 @@
-function goHome() {
-    window.location.href = "index.html";
-}
+document.getElementById("menu").innerHTML = `
+    <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="public/dashboard.html">Dashboard</a></li>
+        <li><a href="public/pricing.html">Pricing</a></li>
+        <li><a href="public/referral.html">Referral</a></li>
+        <li><a href="public/account.html">Account</a></li>
+        <li><a href="public/logout.html">Logout</a></li>
+    </ul>
+`;
