31a29f4355329798eda38e4a6718f7fb3891101e
SRLABREEZE <srlabreeze@gmail.com>
2025-02-25 17:44:02 -0500

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to GhostSpoof</h1>
+            <p>Your ultimate spoofing tool.</p>
+        </div>
+    );
+};
+
+export default Home;
