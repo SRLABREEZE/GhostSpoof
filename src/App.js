62136d5cbe440a44cf9564f8c5cc331d248a8a90
SRLABREEZE <srlabreeze@gmail.com>
2025-02-25 20:11:09 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { AuthProvider } from "./context/AuthContext";
+import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./routes";
 import Navbar from "./components/Navbar";
 
 const App = () => {
     return (
-        <AuthProvider>
+        <Router>
             <Navbar />
             <AppRoutes />
-        </AuthProvider>
+        </Router>
     );
 };
 
